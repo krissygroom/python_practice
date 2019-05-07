28a776c5364fed806a475886ae00fd905bf3dbdb
Krissy Groom <krissydgroom@gmail.com>
2019-05-07 14:42:11 -0700

    Initial Commit for Data Structures and Algorithms Project 0

---
+++ Task1.py
@@ -0,0 +1,83 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import numpy as np
+import time
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+TASK 1:
+How many different telephone numbers are there in the records? 
+Print a message:
+"There are <count> different telephone numbers in the records."
+"""
+
+# check length of each list
+# print(len(texts))
+# print(len(calls))
+
+
+# first combine the two lists for easier use
+def combine_lists(call_list, text_list):
+    """Function to combine two lists of call or text observations"""
+    return call_list + text_list
+
+
+# extract the first and second items of each call/text in the combined list
+def extract_numbers(numbers_list):
+    """Function that extracts the telephone numbers from a list"""
+    new_list1 = [item[0] for item in numbers_list]
+    new_list2 = [item[1] for item in numbers_list]
+    return new_list1 + new_list2
+
+
+# implement the number of different telephone numbers function
+def count_unique_numbers(call_list, text_list):
+    """Function that performs the calculations for finding unique numbers in a list
+    Args:
+        call_list: list of telephone call observations
+        text_list: list of text message observations
+    Returns:
+        An int representing the number of unique telephone numbers in both records
+    """
+    lists_combined = combine_lists(call_list, text_list)
+    all_numbers = extract_numbers(lists_combined)
+
+    # convert all_numbers into numpy array
+    all_numbers = np.asarray(all_numbers)
+    num_unique = np.unique(all_numbers)
+    return len(num_unique)
+
+
+# def test_cases():
+#     practice_texts = [['801 375', '1234', '01-09-2016 06:03:22'],
+#                       ['801 375', '5678', '01-09-2016 06:05:35'],
+#                       ['1234', '9999', '01-09-2016 06:05:35']]
+#
+#     practice_calls = [['801 375', '3333', '01-09-2016 06:01:12', '186'],
+#                       ['3333', '1234', '01-09-2016 06:01:59', '2093'],
+#                       ['9999', '1010', '01-09-2016 06:03:51', '1975']]
+#
+#     return count_unique_numbers(practice_texts, practice_calls)
+
+
+# print(test_cases())
+
+"""Answer to Task 1:
+"""
+# start = time.time()
+unique_numbers = count_unique_numbers(calls, texts)
+print("There are {} different telephone numbers in the records!".format(unique_numbers))
+# end = time.time()
+# print("Time of execution in seconds: {}".format(end - start))
+
+
---
+++ Task2.py
@@ -0,0 +1,81 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
+Print a message:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+September 2016.".
+"""
+
+
+def extract_call_numbers(call_list):
+    """Extracts the numbers and seconds from the call list
+    Args:
+        call_list: a list of tuples of phone number and seconds
+    Returns:
+        a new list of only phone numbers
+    """
+    call_ex1 = [(call[0], call[3]) for call in call_list]
+    call_ex2 = [(call[1], call[3]) for call in call_list]
+    return call_ex1 + call_ex2
+
+
+# ok so now just need to add the seconds time to each of the numbers in the calls list
+# this is how to get the key with the maximum value from the dictionary: max(dict, key=dict.get)
+# create a function to do this:
+def phone_durations(num_list):
+    # create empty dictionary
+    num_dict = {}
+    # iterate through the number list and the seconds and add each phone number with the amount of seconds
+    # to the dictionary if it is not in the dictionary.
+    # if it is in the dictionary, then just add the seconds to its value
+    for i in range(len(num_list)):
+        seconds = int(num_list[i][1])
+        number = num_list[i][0]
+        if number not in num_dict:
+            num_dict[number] = seconds
+        else:
+            num_dict[number] += seconds
+    return num_dict
+
+
+def find_max_call(all_calls):
+    call_list = extract_call_numbers(all_calls)
+    durations_dict = phone_durations(call_list)
+    max_number = max(durations_dict, key=durations_dict.get)
+    max_seconds = durations_dict[max_number]
+    return max_number, max_seconds
+
+
+# practice_calls = [['801 375', '3333', '01-09-2016 06:01:12', '186'],
+#                   ['3333', '1234', '01-09-2016 06:01:59', '2093'],
+#                   ['9999', '1010', '01-09-2016 06:03:51', '1975']]
+#
+#
+# list_example = [('801 375', '186'), ('3333', '2093'), ('9999', '1975'), ('3333', '186'), ('1234', '2093'),
+#                 ('1010', '1975')]
+#
+# print(phone_durations(list_example))
+#
+# print(find_max_call(practice_calls))
+
+"""Answer to Task 1:
+"""
+
+longest_call_number = find_max_call(calls)
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(longest_call_number[0],
+                                                                                          longest_call_number[1]))
+
