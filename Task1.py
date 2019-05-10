00bdc1d15e16d19eedeb74b755eb24130a83216c
Krissy Groom <krissydgroom@gmail.com>
2019-05-10 07:43:16 -0700

    Simplified code in Task 1 to make more efficient

--- Task1.py
+++ Task1.py
@@ -20,62 +20,59 @@ Print a message:
 "There are <count> different telephone numbers in the records."
 """
 
-# check length of each list
-# print(len(texts))
-# print(len(calls))
 
-
-# first combine the two lists for easier use
-def combine_lists(call_list, text_list):
-    """Function to combine two lists of call or text observations"""
-    return call_list + text_list
-
-
-# extract the first and second items of each call/text in the combined list
-def extract_numbers(numbers_list):
-    """Function that extracts the telephone numbers from a list"""
-    new_list1 = [item[0] for item in numbers_list]
-    new_list2 = [item[1] for item in numbers_list]
-    return new_list1 + new_list2
-
-
-# implement the number of different telephone numbers function
-def count_unique_numbers(call_list, text_list):
-    """Function that performs the calculations for finding unique numbers in a list
-    Args:
-        call_list: list of telephone call observations
-        text_list: list of text message observations
-    Returns:
-        An int representing the number of unique telephone numbers in both records
-    """
-    lists_combined = combine_lists(call_list, text_list)
-    all_numbers = extract_numbers(lists_combined)
-
-    # convert all_numbers into numpy array
-    all_numbers = np.asarray(all_numbers)
-    num_unique = np.unique(all_numbers)
-
-    return len(num_unique)
-
-
-# def test_cases():
-#     practice_texts = [['801 375', '1234', '01-09-2016 06:03:22'],
-#                       ['801 375', '5678', '01-09-2016 06:05:35'],
-#                       ['1234', '9999', '01-09-2016 06:05:35']]
+# # first combine the two lists for easier use
+# def combine_lists(call_list, text_list):
+#     """Function to combine two lists of call or text observations"""
+#     return call_list + text_list
 #
-#     practice_calls = [['801 375', '3333', '01-09-2016 06:01:12', '186'],
-#                       ['3333', '1234', '01-09-2016 06:01:59', '2093'],
-#                       ['9999', '1010', '01-09-2016 06:03:51', '1975']]
 #
-#     return count_unique_numbers(practice_texts, practice_calls)
+# # extract the first and second items of each call/text in the combined list
+# def extract_numbers(numbers_list):
+#     """Function that extracts the telephone numbers from a list"""
+#     new_list1 = [item[0] for item in numbers_list]
+#     new_list2 = [item[1] for item in numbers_list]
+#     return new_list1 + new_list2
+#
+#
+# # implement the number of different telephone numbers function
+# def count_unique_numbers(call_list, text_list):
+#     """Function that performs the calculations for finding unique numbers in a list
+#     Args:
+#         call_list: list of telephone call observations
+#         text_list: list of text message observations
+#     Returns:
+#         An int representing the number of unique telephone numbers in both records
+#     """
+#     lists_combined = combine_lists(call_list, text_list)
+#     all_numbers = extract_numbers(lists_combined)
+#
+#     # convert all_numbers into numpy array
+#     all_numbers = np.asarray(all_numbers)
+#     num_unique = np.unique(all_numbers)
+#
+#     return len(num_unique)
 
 
-# print(test_cases())
+# much better approach as suggested by Udacity reviewer!
+# I definitely overcomplicated it in above code, plus is faster in below code.
+def approach_2(calls, texts):
+    records = set()
+    for i in range(len(calls)):
+        records.add(calls[i][0])
+        records.add(calls[i][1])
+    for i in range(len(texts)):
+        records.add(texts[i][0])
+        records.add(texts[i][1])
+    return len(records)
+
 
 """Answer to Task 1:
 """
-unique_numbers = count_unique_numbers(calls, texts)
-print("There are {} different telephone numbers in the records!".format(unique_numbers))
+print("There are {0} different telephone numbers in the records.".format(approach_2(calls, texts)))
+
+# unique_numbers = count_unique_numbers(calls, texts)
+# print("There are {} different telephone numbers in the records!".format(unique_numbers))
 
 
 
